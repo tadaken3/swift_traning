7e86d3888d230e21d9eebd92a2b025592fbeb4cb
tadaken3 <k.tanaka6057@gmail.com>
2020-05-31 17:14:35 +0900

    Initial Commit

---
+++ Hello World/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello World
+//
+//  Created by 田中賢太 on 2020/05/31.
+//  Copyright © 2020 kenta.tanaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
